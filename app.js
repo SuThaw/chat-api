787b91828de69f4c9606b37c708328e493a39c6c
Su Thaw <suthaw524@gmail.com>
2015-04-02 14:12:08 +0630

    allow cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var io = require('socket.io').listen(app.listen(port));
 // Require the configuration and the routes files, and pass
 // the app and io as arguments to the returned functions.
 
-
+require('./config')(app,io);
 require('./routes')(app, io);
 
 console.log('Your application is running on http://localhost:' + port);
